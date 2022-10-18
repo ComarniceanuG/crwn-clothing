ff3805b83cd8bc3c68c3791395beb9863d5b6201
ComarniceanuG <comarniceanugeorge@gmail.com>
2022-10-18 22:35:59 +0300

    first commit

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,95 @@
+import { useState } from 'react';
+ 
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
+import FormInput from '../form-input/form-input.component';
+
+
+import { 
+  signInWithGooglePopup, 
+  signInAuthUserWithEmailAndPassword, 
+  createUserDocumentFromAuth 
+} from '../../utils/firebase/firebase.utils';
+
+import { SignInContainer, ButtonsContainer } from './sign-in-form.styles';
+
+const defaultFormFields = {
+  email: '',
+  password: ''
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const signInWithGoogle = async () => {
+    await signInWithGooglePopup();
+  }
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setFormFields({...formFields, [name]: value});
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await signInAuthUserWithEmailAndPassword(email, password);
+      resetFormFields();
+    } catch (error) {
+      switch (error.code){
+        case 'auth/wrong-password':
+          alert('incorrect password for email');
+          break;
+        case 'auth/user-not-found':
+          alert('no user associated with this email');
+          break;
+        default:
+          console.log(error);
+      }
+    }
+  };
+
+  return (
+    <SignInContainer>
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput 
+          label='Email'
+          type='email' 
+          name='email' 
+          value={email} 
+          onChange={handleChange}
+          required 
+        />
+
+        <FormInput 
+          label='Password'
+          type='password' 
+          name='password' 
+          value={password}
+          onChange={handleChange}
+          required 
+        />
+        
+        <ButtonsContainer>
+          <Button type='submit'>Sign In</Button>
+          <Button 
+            type='button' 
+            buttonType={BUTTON_TYPE_CLASSES.google} 
+            onClick={signInWithGoogle}
+          >
+            Google sign in
+          </Button>
+        </ButtonsContainer>
+      </form>
+    </SignInContainer>
+  );
+};
+
+export default SignInForm;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,102 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs } from 'firebase/firestore';
+
+const config = {
+  apiKey: "AIzaSyAE37Vc6uxyuEemqI5oSI3dZLwZB8rb-l0",
+  authDomain: "crwn-clothing-66199.firebaseapp.com",
+  projectId: "crwn-clothing-66199",
+  storageBucket: "crwn-clothing-66199.appspot.com",
+  messagingSenderId: "223869747839",
+  appId: "1:223869747839:web:427b2f2ce50663856e354a"
+};
+
+firebase.initializeApp(config);
+
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: "select_account"
+});
+
+export const auth = firebase.auth();
+export const signInWithGooglePopup = () => auth.signInWithPopup(googleProvider);
+export const signInWithGoogleRedirect = () => auth.signInWithRedirect(googleProvider);
+export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const batch = writeBatch(db);
+  const collectionRef = collection(db, collectionKey);
+  
+  objectsToAdd.forEach((object) => {
+     const docRef = doc(collectionRef, object.title.toLowerCase());
+     batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log('done');
+};
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories');
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {})
+
+  return categoryMap;
+};
+
+export const createUserDocumentFromAuth = async (
+  userAuth, 
+  additionalInformation = {}
+  ) => {
+    if (!userAuth) return;
+
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    const userSnapshot = await getDoc(userDocRef);
+
+    if (!userSnapshot.exists()) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      
+      try {
+        await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt,
+          ...additionalInformation
+        })
+      }
+      catch (error) {
+        console.log('error creating user', error);
+      }
+    }
+
+    return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await auth.createUserWithEmailAndPassword(email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await auth.signInWithEmailAndPassword(email, password);
+};
+
+export const signOutUser = async () => await auth.signOut();
+
+export const onAuthStateChangedListener = (callback) => auth.onAuthStateChanged(callback);
