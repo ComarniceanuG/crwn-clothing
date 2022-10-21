7c6b188a28c0c52066373a1d9d49ca7467f0bea6
ComarniceanuG <comarniceanugeorge@gmail.com>
2022-10-21 17:15:12 +0300

    added redux-thunk

---
+++ src/store/categories/category.selector.js
@@ -0,0 +1,23 @@
+import { createSelector } from 'reselect';
+
+const selectCategoryReducer = state => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  categories => categories.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  categories => 
+    categories.reduce(
+      (acc, { title, items }) => {
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {})
+);
+
+export const selectCategoriesIsLoading = createSelector(
+  [selectCategoryReducer],
+  categories => categories.isLoading
+);
