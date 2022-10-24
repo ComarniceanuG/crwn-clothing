ab3e766a63e6cebdb9402b29a7dcde6ca64af2c1
ComarniceanuG <comarniceanugeorge@gmail.com>
2022-10-24 16:13:50 +0300

    updating naming convention for payment form container

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,12 @@
-import { useContext } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+
+import { selectCartItems } from '../../store/cart/cart.selector';
+import { setIsCartOpen } from '../../store/cart/cart.action';
 
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
-import { selectCartItems } from '../../store/cart/cart.selector';
+
 
 import {
   CartDropdownContainer,
@@ -13,10 +15,12 @@ import {
 } from './cart-dropdown.styles';
 
 const CartDropdown = () => {
+  const dispatch = useDispatch();
   const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
 
   const goToCheckoutHandler = () => {
+    dispatch(setIsCartOpen(false));
     navigate('/checkout');
   };
 
--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { selectCartTotal } from '../../store/cart/cart.selector';
 import { selectCurrentUser } from '../../store/user/user.selector';
 import { setClearCart } from '../../store/cart/cart.action';
 
+import { FormContainer } from './payment-form.styles';
 import { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
-import { PaymentFormContainer, FormContainer, PaymentButton } from './payment-form.styles';
+import { PaymentButton, PaymentFormContainer } from './payment-form.styles';
 
 const PaymentForm = () => {
   const dispatch = useDispatch();
@@ -18,58 +19,58 @@ const PaymentForm = () => {
   const currentUser = useSelector(selectCurrentUser);
   const [isProcessingPayment, setIsProcessingPayment] = useState(false);
 
-   const paymentHandlder = async (e) => {
+  const paymentHandler = async (e) => {
     e.preventDefault();
-
-    if (!stripe || !elements) return;
-
+    if (!stripe || !elements) {
+      return;
+    }
     setIsProcessingPayment(true);
-
     const response = await fetch('/.netlify/functions/create-payment-intent', {
       method: 'post',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ amount: amount * 100 })
-    }).then(res => res.json());
+      body: JSON.stringify({ amount: amount * 100 }),
+    }).then((res) => {
+      return res.json();
+    });
 
-    const { paymentIntent: { client_secret } } = response;
+    const clientSecret = response.paymentIntent.client_secret;
 
-    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+    const paymentResult = await stripe.confirmCardPayment(clientSecret, {
       payment_method: {
         card: elements.getElement(CardElement),
         billing_details: {
-          name: currentUser ? currentUser.displayName : 'Guest'
-        }
-      }
+          name: currentUser ? currentUser.displayName : 'Yihua Zhang',
+        },
+      },
     });
 
-    dispatch(setClearCart());
     setIsProcessingPayment(false);
 
     if (paymentResult.error) {
-      alert(paymentResult.error);
+      alert(paymentResult.error.message);
     } else {
       if (paymentResult.paymentIntent.status === 'succeeded') {
-        alert('Payment Successful');
+        alert('Payment Successful!');
+        dispatch(setClearCart());
       }
     }
-   };
+  };
 
   return (
     <PaymentFormContainer>
-      <FormContainer onSubmit={paymentHandlder}>
-        <h2>Credit Card Payment: </h2>
+      <FormContainer onSubmit={paymentHandler}>
+        <h2>Credit Card Payment:</h2>
         <CardElement />
-        <PaymentButton 
-          isLoading={isProcessingPayment} 
+        <PaymentButton
           buttonType={BUTTON_TYPE_CLASSES.inverted}
+          isLoading={isProcessingPayment}
         >
-          Pay now
+          Pay Now
         </PaymentButton>
       </FormContainer>
     </PaymentFormContainer>
   );
 };
-
 export default PaymentForm;
